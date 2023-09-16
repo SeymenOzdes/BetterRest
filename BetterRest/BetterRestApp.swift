d36450b5515aad3ae4fbabd31ae1b375c5890e57
SeymenOzdes <ozdesxseymen@gmail.com>
2023-09-16 15:07:51 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Seymen Özdeş on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
